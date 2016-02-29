0ef8e7100b802267ed6d031a4c0d048d14a761eb
Samir El Benna <ingbenna@users.noreply.github.com>
2016-02-29 17:51:28 +0100

    Added mpd start

--- boot_script.sh
+++ boot_script.sh
@@ -3,6 +3,7 @@
 ac_status=$(cat /sys/class/power_supply/AC0/online)
 
 echo $ac_status
+mpd &
 if [ $ac_status -eq 0 ]; then
 	echo -n 2441 > /sys/class/backlight/intel_backlight/brightness
 	cpupower frequency-set -g powersave
