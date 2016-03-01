a95f1e184fe114468ef4202d015167eb4f58c9cf
Samir El Benna <ingbenna@users.noreply.github.com>
2016-03-01 18:24:17 +0100

    Location /etc/acpi/
    Script to handle acpi event by acpid (it's its configuration file)

---
+++ handler.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Default acpi script that takes an entry for all actions
+
+case "$1" in
+    button/power)
+        case "$2" in
+            PBTN|PWRF)
+                logger 'PowerButton pressed'
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
+        esac
+        ;;
+    button/sleep)
+        case "$2" in
+            SLPB|SBTN)
+                logger 'SleepButton pressed'
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
+        esac
+        ;;
+    ac_adapter)
+        case "$2" in
+            AC*|ACAD|ADP0)
+                case "$4" in
+                    00000000)
+                        logger 'AC unpluged'
+			cpupower frequency-set -g powersave
+			echo -n 2441 > /sys/class/backlight/intel_backlight/brightness
+                        ;;
+                    00000001)
+                        logger 'AC pluged'
+			cpupower frequency-set -g performance
+			echo -n 4882 > /sys/class/backlight/intel_backlight/brightness
+                        ;;
+                esac
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
+        esac
+        ;;
+    battery)
+        case "$2" in
+            BAT0)
+                case "$4" in
+                    00000000)
+                        logger 'Battery online'
+                        ;;
+                    00000001)
+                        logger 'Battery offline'
+                        ;;
+                esac
+                ;;
+            CPU0)
+                ;;
+            *)  logger "ACPI action undefined: $2" ;;
+        esac
+        ;;
+    button/lid)
+        case "$3" in
+            close)
+                logger 'LID closed'
+                ;;
+            open)
+		/usr/local/bin/lid_opened_script.zsh
+                logger 'LID opened'
+                ;;
+            *)
+                logger "ACPI action undefined: $3"
+                ;;
+    esac
+    ;;
+    *)
+        logger "ACPI group/action undefined: $1 / $2"
+        ;;
+esac
+
+# vim:set ts=4 sw=4 ft=sh et:
